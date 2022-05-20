6fca5490fee82b417fea45c215bc153c342a905d
Dreezy <bankoleidris@gmail.com>
2022-05-20 17:53:24 +0000

    loaded icons

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 import { Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
---
+++ constants/theme.ts
@@ -0,0 +1,103 @@
+import { Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+export const SIZES = {
+  // global sizes
+  base: 8,
+  font: 14,
+  radius: 12,
+  padding: 24,
+
+  // font sizes
+  h1: 30,
+  h2: 22,
+  h3: 16,
+  h4: 14,
+  body1: 30,
+  body2: 22,
+  body3: 16,
+  body4: 14,
+
+  width,
+  height,
+};
+
+export const COLORS = {
+  red: "#E20000",
+  purple: "#19235B",
+  white: "#fff",
+  offPurple: "#D9DFEB",
+  bg: "#E5E5E5",
+  DarkBlue: "#2D4379",
+  linearGrad_1: "#0D253C",
+  linearGrad_2: "#49B0E2",
+  blue: "#376AED",
+  lightBlue: "#03A9F1",
+  lightBrown: "#F18303",
+  ash: "#7A8194",
+};
+
+export const FONTS = {
+  Inter_Black: require("../assets/fonts/Inter/static/Inter-Black.ttf"),
+  Inter_Bold: require("../assets/fonts/Inter/static/Inter-Bold.ttf"),
+  Inter_ExtraBold: require("../assets/fonts/Inter/static/Inter-ExtraBold.ttf"),
+  Inter_ExtraLight: require("../assets/fonts/Inter/static/Inter-ExtraLight.ttf"),
+  Inter_Light: require("../assets/fonts/Inter/static/Inter-Light.ttf"),
+  Inter_Medium: require("../assets/fonts/Inter/static/Inter-Medium.ttf"),
+  Inter_Regular: require("../assets/fonts/Inter/static/Inter-Regular.ttf"),
+  Inter_SemiBold: require("../assets/fonts/Inter/static/Inter-SemiBold.ttf"),
+  Inter_Thin: require("../assets/fonts/Inter/static/Inter-Thin.ttf"),
+};
+
+export const FontStyles = { normal: "normal", italic: "italic" };
+
+export const FontWeights = {
+  fwNormal: "normal",
+  fwBold: "bold",
+  fw100: "100",
+  fw200: "200",
+  fw300: "300",
+  fw400: "400",
+  fw500: "500",
+  fw600: "600",
+  fw700: "700",
+  fw800: "800",
+  fw900: "900",
+};
+
+export const TextAlignments = {
+  text_auto: "auto",
+  text_left: "left",
+  text_right: "right",
+  text_center: "center",
+  text_justify: "justify",
+};
+
+export const TextDecorationLines = {
+  none: "none",
+  underline: "underline",
+  line_through: "line-through",
+  underline_linethrough: "underline line-through",
+};
+
+export const TextDecorationStyles = {
+  solid: "solid",
+  double: "double",
+  dotted: "dotted",
+  dashed: "dashed",
+};
+
+export const TextTransformations = {
+  text_none: "none",
+  text_uppercase: "uppercase",
+  text_lowercase: "lowercase",
+  text_capitalize: "capitalize",
+};
+
+export const TextAlignmentsVertical = {
+  auto: "auto",
+  top: "top",
+  bottom: "bottom",
+  center: "center",
+};
