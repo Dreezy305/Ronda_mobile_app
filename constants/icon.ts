699b45232f0058fcef595fa5a6b26e8f52d67646
Dreezy <bankoleidris@gmail.com>
2022-05-20 19:49:24 +0000

    icons.ts

--- constants/icon.ts
+++ constants/icon.ts
@@ -1,54 +1,7 @@
-const arrow_forward = require("../assets/icons/arrow-forward.png");
-const arrow_forwardLight = require("../assets/icons/arrow-forwardLight.png");
-const arrow_back = require("../assets/icons/arrow-back.png");
+const arrow_down = require("../assets/icons/arrow-down.png");
+const arrow_up = require("../assets/icons/arrow-up.png");
 const bell = require("../assets/icons/bell.png");
-const camera = require("../assets/icons/Camera.png");
-const chat = require("../assets/icons/Chat.png");
-const chart = require("../assets/icons/Chart.png");
-const close_square = require("../assets/icons/Close_Square.png");
-const trash = require("../assets/icons/delete.png");
-const facebook = require("../assets/icons/facebook.png");
-const google = require("../assets/icons/Google.png");
-const home = require("../assets/icons/Home.png");
-const location = require("../assets/icons/location.svg");
-const upload = require("../assets/icons/Upload.png");
-const myAcc = require("../assets/icons/myacc.png");
-const myFavs = require("../assets/icons/myfavourites.png");
-const nearest = require("../assets/icons/pubaccess.png");
-const organise = require("../assets/icons/Organise.svg");
-const plane = require("../assets/icons/plane.png");
-const twitter = require("../assets/icons/Twitter.png");
-const bag = require("../assets/icons/bag.png");
-const down = require("../assets/icons/down.png");
-const user = require("../assets/icons/user.png");
-
-export default {
-  arrow_forward,
-  arrow_forwardLight,
-  arrow_back,
-  bell,
-  camera,
-
-  chat,
-  chart,
-  close_square,
-  trash,
-
-  facebook,
-  google,
-  home,
-  location,
-
-  upload,
-  myAcc,
-  myFavs,
-  nearest,
-
-  organise,
-  plane,
-  twitter,
-  bag,
-
-  down,
-  user,
-};
+const calendar = require("../assets/icons/calendar.png");
+const cancel = require("../assets/icons/cancel.png");
+const card = require("../assets/icons/card.png");
+const chevron_left = require("../assets/icons/chevron-left.png");
