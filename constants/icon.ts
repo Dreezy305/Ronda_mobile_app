f56398de8864871e6b6f8df91f316856cd6ec0f9
Dreezy <bankoleidris@gmail.com>
2022-05-20 21:06:21 +0000

    icons.ts

--- constants/icon.ts
+++ constants/icon.ts
@@ -45,3 +45,65 @@ const setting_black = require("../assets/icons/setting-black.png");
 const setting = require("../assets/icons/setting.png");
 const shield_color = require("../assets/icons/shield-color.png");
 const shield = require("../assets/icons/shield.png");
+const sqr_menu = require("../assets/icons/sqr-menu.png");
+const switch_icon = require("../assets/icons/switch.png");
+const trash = require("../assets/icons/trash.png");
+const upload = require("../assets/icons/upload.png");
+const user = require("../assets/icons/user.png");
+const wallet = require("../assets/icons/wallet.png");
+
+export {
+  arrow_down,
+  arrow_up,
+  bell,
+  calendar,
+  cancel,
+  card,
+  chevron_left,
+  chevron_up,
+  circle_cancel,
+  circle_check,
+  clip,
+  closeIcon,
+  copy,
+  copy_copy,
+  dart,
+  datepicker,
+  envelop,
+  equivalence,
+  eye,
+  eye_close,
+  finger_print,
+  heart,
+  help,
+  home,
+  info,
+  link,
+  link_to_next,
+  logout,
+  mail,
+  menu,
+  menu_search,
+  menu_slant,
+  menu_trim,
+  not_allowed,
+  pen,
+  phone,
+  phone_ring,
+  plane,
+  referral,
+  refresh,
+  refresh_circle,
+  refresh_timer,
+  search,
+  setting,
+  setting_black,
+  shield,
+  shield_color,
+  sqr_menu,
+  switch_icon,
+  trash,
+  upload,
+  user,
+  wallet,
+};
